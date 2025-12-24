21432e3fe87d9deefed604e68dfb3d7be0f639c2
Pradthelegend <pradeep.studies06@gmail.com>
2025-12-24 23:26:39 +0530

    Added a deleteNode() and minValue()

--- src/datastructures/binarysearchtree/RecursiveBinarySearchTree.java
+++ src/datastructures/binarysearchtree/RecursiveBinarySearchTree.java
@@ -37,6 +37,36 @@ public class RecursiveBinarySearchTree {
         }
     }
 
+    public int minValue(Node currentNode) {
+        while (currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    private Node deleteNode(Node currentNode, int value) {
+        if (currentNode == null) return null;
+
+        if (value < currentNode.value) {
+            currentNode.left = deleteNode(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = deleteNode(currentNode.right, value);
+        } else {
+            if (currentNode.left == null && currentNode.right == null) {
+                return null;
+            } else if (currentNode.left == null) {
+                currentNode = currentNode.right;
+            } else if (currentNode.right == null) {
+                currentNode = currentNode.left;
+            } else {
+                int subTreeMin = minValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                currentNode.right = deleteNode(currentNode.right, subTreeMin);
+            }
+        }
+        return currentNode;
+    }
+
     public void recursiveInsert(int value){
         if (root == null) root = new Node(value);
         recursiveInsert(root,value);
@@ -45,4 +75,7 @@ public class RecursiveBinarySearchTree {
     public boolean recursiveContains(int value){
         return recursiveContains(root,value);
     }
+
+    public void deleteNode(int value) { root = deleteNode(root, value); }
+
 }
